c5df404aaa0f7b3275bf678dda451f1681c2ed1a
tam11a-rit <ibrahim.tamim@reserveitbd.com>
2022-02-05 18:33:30 +0600

    add: added PWA Configurations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <b style={{ marginTop: "2rem" }}>Multi Tac</b>
+        <p>Online Multiplayer Game</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/tam11a/multi-tac"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Repository
         </a>
       </header>
     </div>
