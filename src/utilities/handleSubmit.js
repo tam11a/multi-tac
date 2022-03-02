c71c391840222eefdaca8236a55c67cd79699b67
Ibrahim Sadik Tamim <66836027+tam11a@users.noreply.github.com>
2022-03-02 16:10:07 +0600

    Update handleSubmit.js

--- src/utilities/handleSubmit.js
+++ src/utilities/handleSubmit.js
@@ -1,7 +1,8 @@
 const handleEvent = (event) => {
   let data = {};
-  Array.from(new FormData(event.target).entries()).forEach((e) => {
-    data[e[0]] = e[1];
+  const FormDataHere = new FormData(event.target)
+  Array.from(FormDataHere.entries()).forEach((e) => {
+    data[e[0]] = e[0].includes('[]') ? FormDataHere.getAll(e[0]) : FormDataHere.get(e[0]);
   });
   return data;
 };
@@ -10,5 +11,4 @@ const handleSubmit = (event) => {
   event.preventDefault();
   return handleEvent(event);
 };
-
 export default handleSubmit;
